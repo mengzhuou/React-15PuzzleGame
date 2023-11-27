9757fae8a7a6e2ece285c30acb92a75eb101da8f
mengzhuou <mou30@gatech.edu>
2023-11-27 13:44:04 -0500

    remove database and added app check

--- 15puzzle/src/firebase.js
+++ 15puzzle/src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,9 +17,15 @@ const firebaseConfig = {
   appId: "1:723432157461:web:f59e4a8c200315c2f4b91c",
   measurementId: "G-EG15SBV9Y9"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LcAlB4pAAAAANA34IYdVimzlNMTUhRahISt-GSQ'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 export default getFirestore();
